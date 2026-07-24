edcc5c0dcd0b5c0898577e27658a279241170f8b
DinoXanh <mumcip99@gmail.com>
2026-07-24 19:47:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const movies = [
     title: "Girl und Panzer das Finale Part 1",
     year: 2017,
     genre: ["Anime"],
-    poster: "images/gup1.jpg"
+    poster: "images/part1.webp"
 },
 {
     title: "Girl und Panzer das Finale Part 2",
     year: 2019,
     genre: ["Anime"],
-    poster: "images/gup2.jpg"
+    poster: "images/part2.webp"
 },
 {
     title: "Girl und Panzer das Finale Part 3",
     year: 2021,
     genre: ["Anime"],
-    poster: "images/gup3.jpg"
+    poster: "images/part3.webp"
 },
 {
     title: "Girl und Panzer das Finale Part 4",
     year: 2023,
     genre: ["Anime"],
-    poster: "images/gup4.jpg"
+    poster: "images/part4.webp"
 },
 {
     title: "Girl und Panzer das Finale Part 5",
     year: 2025,
     genre: ["Anime"],
-    poster: "images/gup5.jpg"
+    poster: "images/part5.webp"
 },
 {
     title: "Interstellar",
@@ -39,6 +39,6 @@ const movies = [
     title: "Spider-Man: Brand New Day",
     year: 2026,
     genre: ["Siêu anh hùng","Hành động"],
-    poster: "images/spiderman.jpg"
+    poster: "images/spider.jpg"
 }
 ];
